808dc3a447e936f0e8e91916cc15baf9f6097faa
alaabadra <alaabadra4@gmail.com>
2019-08-03 17:53:33 +0300

    i create fake data and only returned place file products same previous place

--- server/dbPostgress/config/db_fakeData.sql
+++ server/dbPostgress/config/db_fakeData.sql
@@ -7,3 +7,7 @@ INSERT INTO grocery_listItem (id , name_grocery) VALUES
 
 INSERT INTO customer (id,first_name, last_name , email , phone_number , street , city , zip_code , ips_id , grocery_listItem_id ) VALUES 
 (1,'alaa','badra','alaabadra4@gmail.com', 25354482, 'salah','gaza',12334,4566,1);
+INSERT INTO admin (id , name ) VALUES 
+(1,'Olasubomi'); 
+INSERT INTO supplier (id , store_name) VALUES 
+(1, 'Dima');
