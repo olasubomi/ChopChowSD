6110ccc64bb76a7a9fb9a15f6e9b9b11b2e585df
alaabadra <alaabadra4@gmail.com>
2019-08-03 21:32:25 +0300

    edit in file query postgress

--- db/queries/forPostgress/getTableCustomer.js
+++
@@ -1,4 +0,0 @@
-const db_connection = require('../../../db/db_connection.js');
-const getCustomer = (grocery_list_id) => db_connection.query('SELECT * FROM public.customer WHERE public.customer .grocery_list_id = $1', [grocery_list_id]);
-module.exports= getCustomer;
-
