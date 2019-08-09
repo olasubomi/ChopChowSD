149de867b5fa2ced01ecb8942a84d45868cd61d4
alaabadra <alaabadra4@gmail.com>
2019-08-09 16:29:58 +0300

    create file for build schema in mongodb

---
+++ dbMongo/config/db_buildSchema.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+exports.list = mongoose.model('list', new Schema({
+    id: Number,
+    groceryname: String
+}))
+
+exports.Customer = mongoose.model('Customer', new Schema({
+    id: Number,
+    firstname: String,
+    lastname: String,
+    email: String,
+    phoneNumber: Number,
+    street: String,
+    city: String,
+    zipCode: Number,
+    ipsid: Number,
+    listid: [{ type: Schema.Types.ObjectId, ref: 'list' }],
+}))
+
+exports.admin = mongoose.model('admin', new Schema({
+    id: Schema.Types.ObjectId,
+    name: String,
+}))
+
+exports.supplier = mongoose.model('supplier', new Schema({
+    id: Schema.Types.ObjectId,
+    storeName: String,
+}))
+
