39eb597c0a7589708cbfa1b643160895e7b4ff28
alaabadra <alaabadra4@gmail.com>
2019-08-09 16:10:10 +0300

    query getcustomerid will return result --> all customer have same listid in schema cutomer

--- dbPostgress/queries/getDataCustomerId.js
+++ dbPostgress/queries/getDataCustomerId.js
@@ -1,5 +1,5 @@
 const db_connection = require('../config/db_connection');
-exports.getDataCustomerId = (grocery_listitem_id) => db_connection.query('SELECT * FROM customer WHERE grocery_listitem_id = $1', [grocery_listitem_id]);
+exports.getDataCustomerId = (listid) => db_connection.query('SELECT * FROM customer WHERE listid = $1', [listid]);
 
 
 
