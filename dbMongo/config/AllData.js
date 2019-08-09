717f6bf5c4bd6b7af40413486ea523011ae8b0b4
alaabadra <alaabadra4@gmail.com>
2019-08-09 16:37:01 +0300

    file AllData contains on all data that it need in mongodb

---
+++ dbMongo/config/AllData.js
@@ -0,0 +1,11 @@
+
+exports.allData = {
+
+    groceryList: {
+        id: 1,
+        product_name: "Ijebu Garri",
+        product_image: '/images/products/garri.jpg',
+        product_price: 8.99,
+        sizes: ["50lbs"]
+    }
+}
