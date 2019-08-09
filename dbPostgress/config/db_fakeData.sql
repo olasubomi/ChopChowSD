5b45c734d7dcc85cd8a1f07c78b56b7f3242f1b1
alaabadra <alaabadra4@gmail.com>
2019-08-09 16:35:27 +0300

    create file fake data for only test response data

---
+++ dbPostgress/config/db_fakeData.sql
@@ -0,0 +1,65 @@
+INSERT INTO
+  list (id, groceryname)
+VALUES
+  (1, 'pizza');
+INSERT INTO
+  customer (
+    id,
+    firstname,
+    lastname,
+    email,
+    phonenumber,
+    street,
+    city,
+    zipcode,
+    ipsid,
+    listid
+  )
+VALUES
+  (
+    1,
+    'alaa',
+    'badra',
+    'alaabadra4@gmail.com',
+    25354482,
+    'salah',
+    'gaza',
+    12334,
+    4566,
+    1
+  );
+INSERT INTO
+  customer (
+    id,
+    firstname,
+    lastname,
+    email,
+    phonenumber,
+    street,
+    city,
+    zipcode,
+    ipsid,
+    listid
+  )
+VALUES
+  (
+    2,
+    'bbnn',
+    'mmjjll',
+    'im,ll',
+    25354482,
+    'hhhj',
+    'gaza',
+    12334,
+    4566,
+    1
+  );
+INSERT INTO
+  admin (id, name)
+VALUES
+  (1, 'Olasubomi');
+INSERT INTO
+  supplier (id, storename)
+VALUES
+  (1, 'Dima');
+  
