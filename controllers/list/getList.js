ff9b176b3e39e2878aa48a61ad7300ea964bdb30
alaabadra <alaabadra4@gmail.com>
2019-08-10 23:08:07 +0300

    add in file getLists --> cookie for every cusotmer and i put customerId from req.params into parameter getDataCustomerId in which this is query will return result--> data for this customer in which i take from this data --> listid to compare this listid with id in schema list

--- controllers/list/getList.js
+++ controllers/list/getList.js
@@ -1,37 +1,26 @@
 const { list } = require('../../dbMongo/config/db_buildSchema')
-
-const { getDataCustomer } = require('../../dbPostgress/queries/getDataCustomer')
 const { getDataCustomerId } = require('../../dbPostgress/queries/getDataCustomerId')
-const {genCookieCustomer} = require('../authuntication/genCookieCustomer'); 
+const {sign} = require('jsonwebtoken')
 exports.getList = (req, res) => {
+    const { customerId } = req.params
+    const tokenCustomer =   sign(customerId,process.env.SECRET);
+    res.cookie('JWTcustomerId',tokenCustomer,{
+        maxAge: 60 * 60 * 24 * 30,
+        httpOnly: true,
+      });
 
-    getDataCustomer().then(resultCustomer => {
+    getDataCustomerId(customerId).then((result) => {
+        let data = {};
+        data = result.rows[0];
+        return list.find({ id: data.listid })
+            .then(resList => {
+                res.send({
+                    data: resList
+                })
 
-        getDataCustomerId(resultCustomer.rows[0].listid).then((result) => {
-            if(result){
-                const {customerId} = req.params
-                console.log(8888888888888);
-                
-                genCookieCustomer(customerId).then((token)=>{
-                    console.log(6666666666666666);
-                    
-                    res.cookie('customerId',token,{ maxAge: 60 * 60 * 60 });
-                    res.json('done')
-                }).catch(err=>console.log(err)
-                )
-            }
-            let data = {};
-            data = result.rows[0];
-            return list.find({ id: data.listid })
-                .then(resList => {
-                    res.send({
-                        data: resList
-                    })
+            })// data from list that it spcial of this customer
 
-                })// data from list that it spcail of this customer
-        })
+        .catch(() =>  next({ code: 500, msg: 'sorry , found Inernal server error' }));
     })
-        .catch(err => console.log(err))
+
 }
-
-
