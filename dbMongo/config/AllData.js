e74192e85c7fe60ac428aefdb3628e0ccd81f813
alaabadra <alaabadra4@gmail.com>
2019-08-10 13:56:53 +0300

    edit in file AllData.js in which i put in this file require for file schema and insert data in this schema

--- dbMongo/config/AllData.js
+++ dbMongo/config/AllData.js
@@ -1,22 +1,111 @@
 const { list } = require('./db_buildSchema');
-console.log(7777777777);
-
 module.exports = async () => {
-    console.log(1111111111111111);
-    
     const allDataList = [
         {
+            id:1,
             product_name: "Ijebu Garri",
             product_image: '/images/products/garri.jpg',
             product_price: 8.99,
-            sizes:["50lbs"]
+            sizes: ["50lbs"]
+        }, {
+            id:2,
+            product_name: "Brown Beans",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        }, {
+            id:3,
+            product_name: "Honey Beans",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        }, {
+            id:4,
+            product_name: "Spinach",
+            product_image: '/images/products/garri.jpg',
+            product_price: 2.99,
+            sizes: ["0.5lbs"]
         },
         {
-            product_name: "Ijebuhhhh Garri",
+            id:5,
+            product_name: "Kale",
             product_image: '/images/products/garri.jpg',
-            product_price: 88.99,
-            sizes:["50nnlbs"]
+            product_price: 2.99,
+            sizes: ["0.5lbs"]
+        }, {
+            id:6,
+            product_name: "Apples",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 2.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        },
+        {
+            id:6,
+            product_name: "Blueberries",
+            product_image: '/images/products/blueberries.jpg',
+            product_price: 1.00,
+            sizes: ["1lbs"]
+        },
+        {
+            id:7,
+            product_name: "Squash Potatoes",
+            product_image: '/images/products/potatoes.jpg',
+            product_price: 8.99,
+            sizes: ["50lbs"]
+        }, {
+            id:8,
+            product_name: "Purple Onions",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        },
+        {
+            id:8,
+            product_name: "Water",
+            product_image: '/images/products/garri.jpg',
+            product_price: 8.99,
+            sizes: ["50lbs"]
+        }, {
+            id:9,
+            product_name: "Plantain Chips",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        }, {
+            id:10,
+            product_name: "Cashews",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        }, {
+            id:11,
+            product_name: "Peanuts",
+            product_image: '/images/products/garri.jpg',
+            product_price: 8.99,
+            sizes: ["50lbs"]
+        }, {
+            id:12,
+            product_name: "Watermelon",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
+        }, {
+            id:13,
+            product_name: "Pineapple",
+            product_image: '/images/products/beans.jpeg',
+            product_price: 12.00,
+            sizes: ["2 lbs"],
+            price_per_ounce: 3.2
         }
-    ];
+    ]
+
     return list.create(allDataList)
 }
+
