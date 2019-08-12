627f238c684099533e7dbd9e484e3deee65038ec
alaabadra <alaabadra4@gmail.com>
2019-08-12 12:29:53 +0300

    in getlists in list in controllers i deleted part cookie because i created it in another file from the better

--- controllers/list/getList.js
+++ controllers/list/getList.js
@@ -1,19 +1,7 @@
 const { list } = require('../../dbMongo/config/db_buildSchema')
 const { getDataCustomerId } = require('../../dbPostgress/queries/getDataCustomerId')
-const {sign} = require('jsonwebtoken')
 exports.getList = (req, res) => {
-    const { customerId } = req.params
-    if(customerId){
-     const tokenCustomer =   sign(customerId,process.env.SECRET);
-    res.cookie('JWTcustomerId',tokenCustomer,{
-        maxAge: 60 * 60 * 24 * 30,
-        httpOnly: true,
-      });   
-    }else{
-        res.status(401).send(JSON.stringify({msg:'you not authrized in this page'}))
-    }
-    
-
+    const { customerId } = req.params   
     getDataCustomerId(customerId).then((result) => {
         let data = {};
         data = result.rows[0];
@@ -27,5 +15,6 @@ exports.getList = (req, res) => {
 
         .catch(() =>  next({ code: 500, msg: 'sorry , found Inernal server error' }));
     })
+
 
 }
