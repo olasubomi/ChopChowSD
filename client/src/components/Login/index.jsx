28e3ad764d58fd924a9fc680335264c1964b4e45
alaabadra <alaabadra4@gmail.com>
2019-08-12 12:25:52 +0300

    create login in front and store value for every input in state to enter into body infetch

---
+++ client/src/components/Login/index.jsx
@@ -0,0 +1,152 @@
+
+import React from 'react';
+import './style.css';
+import { Form, Button, Container } from 'react-bootstrap';
+
+import { Link, Redirect } from 'react-router-dom';
+import auth from '../../auth/fetchIsAuthunticated';
+
+export default class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    message: '',
+  };
+
+  handleClick = () => {
+    const { email, password } = this.state;
+    // const { setUserInfo } = this.props;
+    if (email && password) {
+      // make a requset to the back with method post and data{email , password}
+      fetch('/login', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password: password,
+        }),
+      })
+        .then(response => {
+          if (response.status !== 200) {
+            this.setState({ message: 'Bad Request , Check username or password ... !!' });
+          }
+          return response.json();
+        })
+        .then(({ data }) => {
+          if (data) {
+            auth.setUserInfo(data);
+            auth.isAuthenticated = true;
+            // setUserInfo(data);
+            const {
+              history: {
+                push
+                
+              },
+            } = this.props;
+           
+            return push('/home');
+          }
+        })
+        .catch(err => {
+          auth.error = err;
+        });
+    } else {
+      this.setState({ message: 'Please enter all fields' });
+    }
+  };
+
+  handleChange = ({ target: { value, name } }) =>
+    this.setState({ [name]: value, message: '' });
+
+  render() {
+    const { location } = this.props;
+    const { email, password, message } = this.state;
+    if (auth.isAuthenticated) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/home',
+          }}
+        />
+      );
+    }
+    return (
+      // <Container>
+      //   <Form className="login__form">
+      //     <h2 className="login__form-title">LOGIN</h2>
+      //     <Form.Group>
+      //       <Form.Label>Email :</Form.Label>
+      //       <Form.Control
+      //         type="text"
+      //         name="email"
+      //         value={email}
+      //         placeholder="Enter your email"
+      //         onChange={this.handleChange}
+      //       />
+      //     </Form.Group>
+      //     <Form.Group>
+      //       <Form.Label>Password :</Form.Label>
+      //       <Form.Control
+      //         type="password"
+      //         name="password"
+      //         value={password}
+      //         placeholder="Enter your password"
+      //         onChange={this.handleChange}
+      //       />
+      //     </Form.Group>
+      //     <p className="message">{message}</p>
+      //     <Button
+      //       type="button"
+      //       className="login__form-btn"
+      //       onClick={this.handleClick}
+      //     >
+      //       Login
+      //     </Button>
+      //     <Form.Text className="login__form__text-muted">
+      //       Don’t have an account?{' '}
+      //       <Link className="link-signup-word" to="/signup">
+      //         sign up
+      //       </Link>
+      //     </Form.Text>
+      //   </Form>
+      // </Container>
+      // <Container>
+        <form className="login__form">
+          <h2 className="login__form-title">LOGIN</h2>
+            <label>Email :</label>
+            <input
+              type="text"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={this.handleChange}
+            />
+            <label>Password :</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={this.handleChange}
+            />
+          <p className="message">{message}</p>
+          <button
+            type="button"
+            className="login__form-btn"
+            onClick={this.handleClick}
+          >
+            Login
+          </button>
+          <div className="login__form__text-muted">
+            Don’t have an account?{' '}
+            <Link className="link-signup-word" to="/signup">
+              sign up
+            </Link>
+          </div>
+        </form>
+    );
+  }
+}
