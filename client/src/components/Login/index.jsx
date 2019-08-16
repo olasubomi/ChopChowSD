cf9286a11870dc1a6ceba15d149e4acc19378b7e
alaabadra <alaabadra4@gmail.com>
2019-08-16 14:54:09 +0300

    style.css in login frontend --> added style for page login

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './style.css';
-// import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container } from 'react-bootstrap';
 
 import { Link, Redirect } from 'react-router-dom';
 
@@ -29,29 +29,21 @@ export default class Login extends React.Component {
         }),
       })
         .then(response => {
-          console.log(54545454545, response.status);
-          
-          if (response.status === 400 || response.status === 401 ||response.status === 404  ) {
+          if (response.status === 400  ||response.status === 404  ) {
             this.setState({ message: 'Bad Request , Check username or password ... !!' });
+          }else if( response.status === 401){
+            this.setState({ message: 'you are UnAuthorized' });
           }else if( response.status === 500){
             this.setState({message:'sorry , internal server error'})
           }else{
-
             this.setState({ message: 'login sucessfully ', });
-            // return response.json();
             const {
               history: {
                 push
-                
               },
           } = this.props;
-         console.log('ppppppp',this.props);
-         
           return push('/api/grocery');
           }
-
-          
-
         })
        
   
@@ -64,83 +56,49 @@ export default class Login extends React.Component {
     this.setState({ [name]: value, message: '' });
 
   render() {
-    const { location } = this.props;
     const { email, password, message } = this.state;
     
     return (
-      // <Container>
-      //   <Form className="login__form">
-      //     <h2 className="login__form-title">LOGIN</h2>
-      //     <Form.Group>
-      //       <Form.Label>Email :</Form.Label>
-      //       <Form.Control
-      //         type="text"
-      //         name="email"
-      //         value={email}
-      //         placeholder="Enter your email"
-      //         onChange={this.handleChange}
-      //       />
-      //     </Form.Group>
-      //     <Form.Group>
-      //       <Form.Label>Password :</Form.Label>
-      //       <Form.Control
-      //         type="password"
-      //         name="password"
-      //         value={password}
-      //         placeholder="Enter your password"
-      //         onChange={this.handleChange}
-      //       />
-      //     </Form.Group>
-      //     <p className="message">{message}</p>
-      //     <Button
-      //       type="button"
-      //       className="login__form-btn"
-      //       onClick={this.handleClick}
-      //     >
-      //       Login
-      //     </Button>
-      //     <Form.Text className="login__form__text-muted">
-      //       Don’t have an account?{' '}
-      //       <Link className="link-signup-word" to="/signup">
-      //         sign up
-      //       </Link>
-      //     </Form.Text>
-      //   </Form>
-      // </Container>
-      // <Container>
-        <form className="login__form">
+      <Container>
+        <Form className="login__form">
           <h2 className="login__form-title">LOGIN</h2>
-            <label>Email :</label>
-            <input
+          <Form.Group>
+            <Form.Label>Email :</Form.Label>
+            <Form.Control
               type="text"
               name="email"
               value={email}
               placeholder="Enter your email"
               onChange={this.handleChange}
             />
-            <label>Password :</label>
-            <input
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password :</Form.Label>
+            <Form.Control
               type="password"
               name="password"
               value={password}
               placeholder="Enter your password"
               onChange={this.handleChange}
             />
+          </Form.Group>
           <p className="message">{message}</p>
-          <button
+          <Button
             type="button"
             className="login__form-btn"
             onClick={this.handleClick}
           >
             Login
-          </button>
-          <div className="login__form__text-muted">
+          </Button>
+          <Form.Text className="login__form__text-muted">
             Don’t have an account?{' '}
             <Link className="link-signup-word" to="/signup">
               sign up
             </Link>
-          </div>
-        </form>
+          </Form.Text>
+        </Form>
+      </Container>
     );
   }
 }
+
