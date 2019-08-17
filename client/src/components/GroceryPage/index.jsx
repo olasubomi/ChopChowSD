b80f572109bbe4dbe182f0511c6e00b3352adb48
alaabadra <alaabadra4@gmail.com>
2019-08-18 02:41:16 +0300

    edit in path push from login

---
+++ client/src/components/GroceryPage/index.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import './style.css';
+import PageTitle from '../CommonComponents/PageTitle'
+import { Spinner } from 'react-bootstrap'
+import { Row, Container, Alert, Card, Col } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+
+
+export default class GroceryPage extends React.Component {
+    state = {
+        valueData: null,
+        isAuthenticated: false,
+        customerId: null,
+        message: null,
+
+    }
+    componentDidMount() {
+        fetch('/api/grocery', {
+            method: 'GET',
+            credentials: 'same-origin',
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+
+            .then(res => {
+                res.json().then(response => {
+                    if (response.success && response.data) {
+                        this.setState({ isAuthenticated: true })
+                        this.setState({ customerId: response.data })
+                        const { customerId } = this.state;
+                        fetch(`/getLists/${customerId}`, {
+                            method: 'GET',
+                            credentials: 'same-origin',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+
+                        })
+                            .then(res => res.json())
+                            .then(response => {
+                                if (response) {
+                                    this.setState({ valueData: response.data })
+                                }
+                            }).catch(() => {
+                                this.setState({ message: 'Sorry , Internal Server ERROR' })
+                            })
+                    } else {
+                        this.setState({ isAuthenticated: false })
+                    }
+                })
+            });
+    }
+
+
+
+    render() {
+        const { valueData, message, isAuthenticated } = this.state;
+        return (
+            <>
+                {isAuthenticated ? (
+                    <Link to="/api/grocery" className="w3-bar-item w3-button w3-hover-orange w3-mobile">Grocery List</Link>
+
+                ) : <div>WELCOME To Your Page</div>}
+                <PageTitle title="My List in Progcery Page" />
+                <Container className="page__container">
+                    {message && <Alert variant="danger">{message}</Alert>}
+                    {valueData ? (
+                        <>
+                            <Card className="card-image">
+                                {valueData.product_image}
+                            </Card>
+                            <Col xs={12} md={6} lg={3} key={valueData.id}>
+                                <Card className="yourlist__card" key={valueData.id} >
+                                    <Card.Header className="yourlist__card-header">
+                                        <div>No.List>>{valueData.id}>></div>
+                                        Name Product > {valueData.product_name}>
+                                </Card.Header>
+                                    <Card.Text className="yourlist__card-text">
+                                        Product Price >>  {valueData.product_price}>>
+                                </Card.Text>
+                                    <Card.Text className="yourlist__card-text">
+                                        Product Size >> {valueData.sizes} >>
+                                </Card.Text>
+                                </Card>
+
+                            </Col>
+
+
+
+                        </>
+                    ) : <Spinner animation="border" variant="info" />
+                    }
+                </Container>
+
+
+                <div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
+            </>
+        )
+    }
+}
+
+
+
+
--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -9,13 +9,13 @@ export default class Login extends React.Component {
   state = {
     email: '',
     password: '',
-    message: '',
+    messageErr:false,
+    messageSuccess:false,
+    isLogged: false,
   };
 
   handleClick = () => {
-    const {location} = this.props
     const { email, password } = this.state;
-    // const { setUserInfo } = this.props;
     if (email && password) {
       // make a requset to the back with method post and data{email , password}
       fetch('/api/login', {
@@ -30,75 +30,73 @@ export default class Login extends React.Component {
         }),
       })
         .then(response => {
-          if (response.status === 400  ||response.status === 404  ) {
-            this.setState({ message: 'Bad Request , Check username or password ... !!' });
-          }else if( response.status === 401){
-            this.setState({ message: 'you are UnAuthorized' });
-          }else if( response.status === 500){
-            this.setState({message:'Sorry , Internal Server ERROR'})
-          }else{
-            this.setState({ message: 'login sucessfully ', });
-            const {
-              history: {
-                push
-              },
-          } = this.props;
-          return push('/api/grocery');
+          if (response.status === 400 || response.status === 404) {
+            this.setState({ messageErr: 'Bad Request , Check username or password ... !!' });
+          } else if (response.status === 401) {
+            this.setState({ messageErr: 'you are UnAuthorized' });
+          } else if (response.status === 500) {
+            this.setState({ messageErr: 'Sorry , Internal Server ERROR' })
+          } else {
+            this.setState({messageErr:''});
+            this.setState({ messageSuccess: 'login sucessfully '});
+            return window.location.href = '/api/grocery'
           }
         })
-       
-  
+
+
     } else {
-      this.setState({ message: 'Please enter all fields' });
+      this.setState({ messageErr: 'Please enter all fields' });
     }
   };
 
   handleChange = ({ target: { value, name } }) =>
-    this.setState({ [name]: value, message: '' });
+    this.setState({ [name]: value });
 
   render() {
-    const { email, password, message } = this.state;
-    
+    const { email, password, messageErr, messageSuccess } = this.state;
     return (
-      <Container>
-        <Form className="login__form">
-          <h2 className="login__form-title">LOGIN</h2>
-          <Form.Group>
-            <Form.Label>Email :</Form.Label>
-            <Form.Control
-              type="text"
-              name="email"
-              value={email}
-              placeholder="Enter your email"
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Password :</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Enter your password"
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <p className="message">{message}</p>
-          <Button
-            type="button"
-            className="login__form-btn"
-            onClick={this.handleClick}
-          >
-            Login
+      <>
+        <Container>
+          <Form className="login__form">
+            <h2 className="login__form-title">LOGIN</h2>
+            <Form.Group>
+              <Form.Label>Email :</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password :</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Enter your password"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+                <p className="msg-success">{messageSuccess}</p>
+                <p className="msg-err">{messageErr}</p> 
+            <Button
+              type="button"
+              className="login__form-btn"
+              onClick={this.handleClick}
+            >
+              Login
           </Button>
-          <Form.Text className="login__form__text-muted">
-            Don’t have an account?{' '}
-            <Link className="link-signup-word" to="/signup">
-              sign up
+            <Form.Text className="login__form__text-muted">
+              Don’t have an account?{' '}
+              <Link className="link-signup-word" to="/signup">
+                sign up
             </Link>
-          </Form.Text>
-        </Form>
-      </Container>
+            </Form.Text>
+          </Form>
+        </Container>
+      </>
     );
   }
 }
