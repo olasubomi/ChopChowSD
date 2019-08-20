52d548ac13a5d012f6dcbcb1b88a03be4942a7f1
alaabadra <alaabadra4@gmail.com>
2019-08-20 12:41:21 +0300

    file createitem --> add query createCollection  in which i put result from db from this query as API  through res.send

--- controllers/list/createList.js
+++ controllers/list/createList.js
@@ -1,11 +1,12 @@
-const {createList} = require('../../dbMongo/queries/createList');
-module.exports=(req,res)=>{
-    createList()
-    .then((result)=>{
-        res.send({
-            data:result.rows
-        })
-        .catch(()=>next({code:500,msg:'internal server error'}))
-    })
-}
 
+const { list } = require('../../db/dbMongo/config/db_buildSchema')
+
+module.exports=(req,res)=>{
+   list.createCollection({id:Number,product_name:String,product_image:String,product_price:Number,sizes:Number})
+   .then(()=>{
+       res.send({
+           data:'create table list successfully'
+       })
+       .catch(()=>next({code:500,msg:'internal server error'}))
+   })
+}
