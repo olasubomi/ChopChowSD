41f6dc914436fb356cd7d50289250ff5c04b2986
alaabadra <alaabadra4@gmail.com>
2019-08-20 12:47:17 +0300

    edit in file index.js in list in controllers

--- controllers/list/index.js
+++ controllers/list/index.js
@@ -1,7 +1,7 @@
 
 const appendItem = require('./appendItem');
 const deleteItem = require('./deleteItem');
-const createList = require('./createList');
+const createList = require('./removeItem');
 const removeList = require('./removeList');
 
 module.exports = {
