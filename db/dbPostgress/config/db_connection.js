aa5290e55d5cf4ec034ca99b3f78d16fcb33d727
Olasubomi Awokoya <olasubomi.awokoya@hotmail.com>
2019-10-24 11:51:48 -0700

    edit in url db pg

--- db/dbPostgress/config/db_connection.js
+++ db/dbPostgress/config/db_connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const { parse } = require('url');
 require('dotenv').config();
 
- let { DATABASE_URL: dbUrl } = process.env;
+ let { HEROKU_POSTGRESQL_GREEN_URL: dbUrl } = process.env;
 
  const params = parse(dbUrl);
 const {
