d162542001743d08f1ce9385a4b97788562fa952
POOJAKGUPTA <poojakgupta4@gmail.com>
2020-04-24 12:19:09 +0530

    new logic for instruction slider

--- client/src/text_slide.js
+++ client/src/text_slide.js
@@ -14,100 +14,53 @@ class TextSlider extends Component {
       base_index: 0,
       index: 0
     };
-    this.handleSelect = this.handleSelect.bind(this);
-    this.updateInstructionsDisplayBaseIndex = this.updateInstructionsDisplayBaseIndex.bind(
-      this
-    );
+    this.decrease = this.decrease.bind(this);
+    this.increase = this.increase.bind(this);
   }
 
-  handleSelect(selectedIndex) {
-    this.setState({ index: selectedIndex + 1 });
-  }
+  decrease = () => {
+    if (this.state.increment > 0) {
+      this.setState(prevState => ({ increment: prevState.increment - 1 }));
+    }
+  };
 
-  updateInstructionsDisplayBaseIndex(event) {
-    console.log(event.target.innerText);
-    var button = event.target.innerText;
-    // var regExp = "/^w+[ ]/d  $/";
-    // var slide_index = button.match(regExp);
-    //console.log(slide_index);
-    var last_chars = button.slice(6, 7);
+  increase = () => {
+    const {increment} = this.state
 
-    var slide_num = Number(last_chars);
+    const { mealPrep1, value } = this.props;
+    const instructionsLength = value.instructions.length;
+    if((instructionsLength-2)/2 <= this.state.increment){
+      return null
+    }
+    else {
+      this.setState({ increment: this.state.increment + 1 });
+    }
+  };
 
-    this.setState({ base_index: slide_num * 3 });
-    //var base_index = slide_num*3;
-    //console.log("Updating base index on click to: " +this.state.base_index);
-  }
+ 
 
   render() {
-    const { mealPrep1, value } = this.props;
-    console.log({ mealPrep1 }, "pppppppppp");
-    var popUpSlides = [];
-    var carouselSlides = [];
-    var i;
-    const instructionsLength = value.instructions.length;
+    const { increment} = this.state
 
-    for (i = 0; i < instructionsLength / 3; i++) {
-      popUpSlides.push(
-        <button key={i} onClick={this.updateInstructionsDisplayBaseIndex}>
-          Slide {i}{" "}
-        </button>
-      );
-    }
-
-    console.log("instructions length: " + instructionsLength);
-    for (i = 0; i <= instructionsLength / 2; i++) {
-      console.log("i is :" + i);
-      if (mealPrep1[((i * 2) + 1)] !== undefined) {
-        console.log("Comes in here with 3 instructions for slide #: " + i);
-        carouselSlides.push(
-          <Carousel.Item>
-            <img
-              className="img-responsive imageHeighgt"
-              src="https://www.beautycolorcode.com/f6f0f0-2880x1800.png"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <div id="mealPrepChunk">
-                {/* manually add intstruction numbering for display as list */}
-                {(i * 2) + 1}. {mealPrep1[(i * 2) + 0]}
-                {(i * 2) + 2}. {mealPrep1[(i * 2) + 1]}
-                {/* {(i * 3) + 3}. {mealPrep1[(i * 3) + 2]} */}
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-        )
-      }
-      else {
-        if (mealPrep1[(i * 2)] !== undefined) {
-          console.log("Comes in here with 2 instructions for slide #: " + i);
-
-          carouselSlides.push(
-            <Carousel.Item>
-              <img
-                className="img-responsive imageHeighgt"
-                src="https://www.beautycolorcode.com/f6f0f0-2880x1800.png"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <div id="mealPrepChunk">
-                  {(i * 2) + 0}. {mealPrep1[(i * 2) + 0]}
-                  {/* {(i * 3) + 1}. {mealPrep1[(i * 3) + 1]} */}
-                </div>
-              </Carousel.Caption>
-            </Carousel.Item>
-          )
-        }
-      }
-    }
+    const { mealPrep1 } = this.props;
 
     return (
-
       <div>
         <b>Instructions</b>< br></br>
-        <Carousel onSelect={this.handleSelect}>
-          {carouselSlides}
-        </Carousel>
+        <div className="row">
+          <div className="col-md-2 col-xs-2">
+          <span aria-hidden="true" class="carousel-control-prev-icon" onClick={this.decrease}></span>
+          </div>
+         <div className="col-md-8 col-xs-8"> 
+         <div id="mealPrepChunk">
+                 <li>{mealPrep1[(increment * 2) + 0]}</li>
+                 {mealPrep1[(increment * 2) + 1] ? <li>{mealPrep1[(increment * 2) + 1]} </li> : ' ' }
+              </div>  
+            </div>
+          <div className="col-md-2 col-xs-2">
+          <span aria-hidden="true" class="carousel-control-next-icon" onClick={this.increase}></span>
+          </div>
+        </div>
       </div>
 
 
