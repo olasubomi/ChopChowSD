5fee70f970eb472ee344fd44210f79fa125b8c93
olasubomi <olasubomi.awokoya@hotmail,com>
2021-11-23 15:12:01 -0800

    removing syntax error exporting s3 library from handler file

--- db/aws3/addMealInstructionContent.js
+++ db/aws3/addMealInstructionContent.js
@@ -7,8 +7,6 @@ const PutObjectCommand = require ('@aws-sdk/client-s3');
 const REGION = process.env.S3_REGION; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({ region: REGION });
-export { s3Client };
-
 exports.addMealInstructionContent = (req, res) =>{
 // Submittting meal images/videos to specific User and Admin access policy buckets,  
 
