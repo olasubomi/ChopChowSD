47c0549384d942d7b7447deab2ec320c0fce2ee2
olasubomi <olasubomi.awokoya@hotmail,com>
2021-11-23 15:28:57 -0800

    Pushing latest s3 configs for testing

--- db/aws3/addMealInstructionContent.js
+++ db/aws3/addMealInstructionContent.js
@@ -1,16 +1,12 @@
 // Import required AWS SDK clients and commands for Node.js.
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-const S3Client = require ('@aws-sdk/client-s3');
+import { s3Client } from "./s3Client";
 const PutObjectCommand = require ('@aws-sdk/client-s3');
 
-// Set the AWS Region.
-const REGION = process.env.S3_REGION; //e.g. "us-east-1"
-// Create an Amazon S3 service client object.
-const s3Client = new S3Client({ region: REGION });
+
 exports.addMealInstructionContent = (req, res) =>{
 // Submittting meal images/videos to specific User and Admin access policy buckets,  
 
-// Set the parameters
+// Set parameters for post request 
 const params = {
   Bucket: process.env.S3_BUCKET, // The name of the bucket. For example, 'sample_bucket_101'.
   Key: req.mealContentName, // The name of the object. For example, 'sample_upload.txt'.
