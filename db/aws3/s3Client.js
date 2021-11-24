e0b125f05213c8f9f4c19355d5ae49913f9956e5
olasubomi <olasubomi.awokoya@hotmail,com>
2021-11-23 16:35:24 -0800

    Properly calling an s3Client function from the s3 object to connect to buckets

--- db/aws3/s3Client.js
+++ db/aws3/s3Client.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const  S3Client = require("@aws-sdk/client-s3");
+const S3 = require("@aws-sdk/client-s3");
 // Set the AWS Region.
 const REGION = process.env.S3_REGION; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
-const s3Client = new S3Client({ region: REGION });
+const s3Client = new S3.S3Client({ region: REGION });
 exports.s3Client = s3Client;
