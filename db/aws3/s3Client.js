ccb80970f157b82e41c8666f80ea5d983585f1a5
olasubomi <olasubomi.awokoya@hotmail,com>
2021-11-23 16:15:47 -0800

    exporting s3client helper file as a module

--- db/aws3/s3Client.js
+++ db/aws3/s3Client.js
@@ -4,4 +4,4 @@ const  S3Client = require("@aws-sdk/client-s3");
 const REGION = process.env.S3_REGION; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({ region: REGION });
-export { s3Client };
+exports.s3Client = s3Client;
